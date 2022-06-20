93224649e9266fa720f818db0f486ee1ab497ab6
zaizai <327120052@qq.com>
2022-06-20 10:33:51 +0800

    20220620 LeetCode 0508 出现次数最多的子树元素和

---
+++ LeetCode 0508 出现次数最多的子树元素和.py
@@ -0,0 +1,29 @@
+from collections import Counter
+
+
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def findFrequentTreeSum(self, root: TreeNode) -> List[int]:
+        cnt = Counter()
+
+        def dfs(node: TreeNode) -> int:
+            if node is None:
+                return 0
+            sum = node.val + dfs(node.left) + dfs(node.right)
+            cnt[sum] += 1
+            return sum
+
+        dfs(root)
+
+        maxCnt = max(cnt.values())
+        return [k for k, v in cnt.items() if v == maxCnt]
