e102ff4b8e0449681d3734e7eb77bd22baf8aa17
zaizai <327120052@qq.com>
2022-06-23 20:23:13 +0800

    20220623 LeetCode 0030 串联所有单词的子串 Unsolved

---
+++ LeetCode 0030 串联所有单词的子串 Unsolved.py
@@ -0,0 +1,31 @@
+from typing import List, Counter
+
+
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        res = []
+        m, n, ls = len(words), len(words[0]), len(s)
+        for i in range(n):
+            if i + m * n > ls:
+                break
+            differ = Counter()
+            for j in range(m):
+                word = s[i + j * n: i + (j + 1) * n]
+                differ[word] += 1
+            for word in words:
+                differ[word] -= 1
+                if differ[word] == 0:
+                    del differ[word]
+            for start in range(i, ls - m * n + 1, n):
+                if start != i:
+                    word = s[start + (m - 1) * n: start + m * n]
+                    differ[word] += 1
+                    if differ[word] == 0:
+                        del differ[word]
+                    word = s[start - n: start]
+                    differ[word] -= 1
+                    if differ[word] == 0:
+                        del differ[word]
+                if len(differ) == 0:
+                    res.append(start)
+        return res
