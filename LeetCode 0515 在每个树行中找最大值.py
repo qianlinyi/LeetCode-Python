ae248b4e0b0ca74c76a47bd60f66a92b6f35a458
zaizai <327120052@qq.com>
2022-06-24 10:47:39 +0800

    20220624 LeetCode 0515 在每个树行中找最大值

---
+++ LeetCode 0515 在每个树行中找最大值.py
@@ -0,0 +1,28 @@
+from cmath import inf
+from typing import Optional, List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# BFS
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        ans, q = [], [root]
+        if not root:
+            return ans
+        while q:
+            mx, siz, tmp, q = -inf, len(q), q, []
+            for node in tmp:
+                mx = max(mx, node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            ans.append(mx)
+        return ans
