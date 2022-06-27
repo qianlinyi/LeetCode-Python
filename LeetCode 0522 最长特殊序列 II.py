d77cb3408910a999a67e72c996f342124cf2ee59
zaizai <327120052@qq.com>
2022-06-27 10:14:24 +0800

    20220627 LeetCode 0522 最长特殊序列 II

---
+++ LeetCode 0522 最长特殊序列 II.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def findLUSlength(self, strs: List[str]) -> int:
+        def is_subseq(s: str, t: str) -> bool:
+            pt_s = pt_t = 0
+            while pt_s < len(s) and pt_t < len(t):
+                if s[pt_s] == t[pt_t]:
+                    pt_s += 1
+                pt_t += 1
+            return pt_s == len(s)
+
+        ans = -1
+        for i, s in enumerate(strs):
+            flag = True
+            for j, t in enumerate(strs):
+                if i != j and is_subseq(s, t):
+                    flag = False
+                    break
+            if flag:
+                ans = max(ans, len(s))
+        return ans
