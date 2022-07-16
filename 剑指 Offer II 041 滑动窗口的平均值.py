33e7b3310d15e6d601e20f227b89b6c1dd5091b4
zaizai <327120052@qq.com>
2022-07-16 23:26:21 +0800

    20220716 剑指 Offer II 041 滑动窗口的平均值

---
+++ 剑指 Offer II 041 滑动窗口的平均值.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+
+class MovingAverage:
+
+    def __init__(self, size: int):
+        self.window = deque()
+        self.size = size
+
+    def next(self, val: int) -> float:
+        if len(self.window) == self.size:
+            self.window.popleft()
+        self.window.append(val)
+        return sum(self.window) / len(self.window)
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
