2693c4dcac9d413c0be102869c01dadda267add2
zaizai <327120052@qq.com>
2022-09-10 10:27:33 +0800

    20220910 LeetCode 0669 修剪二叉搜索树

---
+++ LeetCode 0669 修剪二叉搜索树.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# DFS
+class Solution:
+    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        if root.val < low:
+            return self.trimBST(root.right, low, high)
+        if root.val > high:
+            return self.trimBST(root.left, low, high)
+        ans = TreeNode(root.val, self.trimBST(root.left, low, high), self.trimBST(root.right, low, high))
+        return ans
