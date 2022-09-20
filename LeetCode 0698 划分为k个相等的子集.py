4e5dd7ac4039ed0765e63b7f5f529436bdf10232
zaizai <327120052@qq.com>
2022-09-20 10:06:05 +0800

    20220920 LeetCode 0698 划分为k个相等的子集

---
+++ LeetCode 0698 划分为k个相等的子集.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+# 状态压缩 DP
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        all = sum(nums)
+        if all % k:
+            return False
+        per = all // k
+        nums.sort()
+        if nums[-1] > per:
+            return False
+        n = len(nums)
+        dp = [False for _ in range(1 << n)]
+        dp[0] = True
+        cursum = [0 for _ in range(1 << n)]
+        for i in range(0, 1 << n):
+            if not dp[i]:
+                continue
+            for j in range(n):
+                if cursum[i] + nums[j] > per:
+                    break
+                if (i >> j & 1) == 0:
+                    next = i | (1 << j)
+                    if not dp[next]:
+                        cursum[next] = (cursum[i] + nums[j]) % per
+                        dp[next] = True
+        return dp[(1 << n) - 1]
