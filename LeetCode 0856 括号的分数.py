bcbf5a2c19942839b110a56781e0219cc77844e3
zaizai <327120052@qq.com>
2022-10-09 15:48:10 +0800

    20221009 LeetCode 0856 括号的分数

---
+++ LeetCode 0856 括号的分数.py
@@ -0,0 +1,10 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        st = [0]
+        for c in s:
+            if c == '(':
+                st.append(0)
+            else:
+                v = st.pop()
+                st[-1] += max(2 * v, 1)
+        return st[-1]
