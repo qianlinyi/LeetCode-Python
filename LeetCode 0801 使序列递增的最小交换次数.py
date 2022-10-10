7ddcf13c22b12e1935957f913ee5a58d0fd536b7
zaizai <327120052@qq.com>
2022-10-10 09:35:47 +0800

    20221010 LeetCode 0801 使序列递增的最小交换次数

---
+++ LeetCode 0801 使序列递增的最小交换次数.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+# DP
+class Solution:
+    def minSwap(self, nums1: List[int], nums2: List[int]) -> int:
+        n = len(nums1)
+        a, b = 0, 1
+        for i in range(1, n):
+            at, bt = a, b
+            a = b = n
+            if nums1[i] > nums1[i - 1] and nums2[i] > nums2[i - 1]:
+                a = min(a, at)
+                b = min(b, bt + 1)
+            if nums1[i] > nums2[i - 1] and nums2[i] > nums1[i - 1]:
+                a = min(a, bt)
+                b = min(b, at + 1)
+        return min(a, b)
