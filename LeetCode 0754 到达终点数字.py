c44a51b0c45a1600af5ca05e176d14a781f6c3b0
zaizai <327120052@qq.com>
2022-11-04 10:22:43 +0800

    20221104 LeetCode 0754 到达终点数字

---
+++ LeetCode 0754 到达终点数字.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reachNumber(self, target: int) -> int:
+        ans = 0
+        target = abs(target)
+        while target > 0:
+            ans += 1
+            target -= ans
+        return ans if target % 2 == 0 else ans + 1 + ans % 2
