ecb1fa85bb72d341a0bc8de9b0144fb3a8616c2d
zaizai <327120052@qq.com>
2022-11-17 08:23:17 +0800

    20221117 LeetCode 0792 匹配子序列的单词数 二分

---
+++ LeetCode 0792 匹配子序列的单词数.py
@@ -0,0 +1,22 @@
+from bisect import bisect_right
+
+
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        pos = defaultdict(list)
+        for i, c in enumerate(s):
+            pos[c].append(i)
+        ans = len(words)
+        for word in words:
+            if len(word) > len(s):
+                ans -= 1
+                continue
+            p = -1
+            for c in word:
+                ps = pos[c]
+                j = bisect_right(ps, p)
+                if j == len(ps):
+                    ans -= 1
+                    break
+                p = ps[j]
+        return ans
