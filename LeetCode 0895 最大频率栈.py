caef45b0c7a08f27fd15fbdf24eb553e6e1db1a2
zaizai <327120052@qq.com>
2022-11-30 10:42:23 +0800

    20221130 LeetCode 0895 最大频率栈 哈希表+栈

---
+++ LeetCode 0895 最大频率栈.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+
+class FreqStack:
+
+    def __init__(self):
+        self.freq = defaultdict(int)
+        self.group = defaultdict(list)
+        self.maxFreq = 0
+
+    def push(self, val: int) -> None:
+        self.freq[val] += 1
+        self.group[self.freq[val]].append(val)
+        self.maxFreq = max(self.maxFreq, self.freq[val])
+
+    def pop(self) -> int:
+        val = self.group[self.maxFreq].pop()
+        self.freq[val] -= 1
+        if len(self.group[self.maxFreq]) == 0:
+            self.maxFreq -= 1
+        return val
+
+# Your FreqStack object will be instantiated and called as such:
+# obj = FreqStack()
+# obj.push(val)
+# param_2 = obj.pop()
