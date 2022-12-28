040df26af29e7cb51ddf596437c18bcd668da62a
zaizai <327120052@qq.com>
2022-12-28 11:06:38 +0800

    20221228 LeetCode 1750 删除字符串两端相同字符后的最短长度 双指针

---
+++ LeetCode 1750 删除字符串两端相同字符后的最短长度.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        left, right = 0, len(s) - 1
+        while left < right and s[left] == s[right]:
+            c = s[left]
+            while left <= right and s[left] == c:
+                left += 1
+            while right >= left and s[right] == c:
+                right -= 1
+        return right - left + 1
