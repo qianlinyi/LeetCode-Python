f297e3877dfd00ed767df20b7801799ea3dd8c5d
zaizai <327120052@qq.com>
2023-01-04 10:01:50 +0800

    20230104 LeetCode 0221 最大正方形 动态规划

---
+++ LeetCode 0221 最大正方形.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return 0
+        maxSide = 0
+        r, c = len(matrix), len(matrix[0])
+        dp = [[0] * c for _ in range(r)]
+        for i in range(r):
+            for j in range(c):
+                if matrix[i][j] == '1':
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+                    maxSide = max(maxSide, dp[i][j])
+        return maxSide * maxSide
