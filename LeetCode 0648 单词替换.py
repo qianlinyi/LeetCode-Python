9a4faf7389c3541045449d237f79e073e4477043
zaizai <327120052@qq.com>
2023-01-07 10:21:54 +0800

    20230107 LeetCode 0648 单词替换 字典树

---
+++ LeetCode 0648 单词替换.py
@@ -0,0 +1,21 @@
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        trie = {}
+        for word in dictionary:
+            cur = trie
+            for c in word:
+                if c not in cur:
+                    cur[c] = {}
+                cur = cur[c]
+            cur['#'] = {}
+        words = sentence.split(' ')
+        for i, word in enumerate(words):
+            cur = trie
+            for j, c in enumerate(word):
+                if '#' in cur:
+                    words[i] = word[:j]
+                    break
+                if c not in cur:
+                    break
+                cur = cur[c]
+        return ' '.join(words)
