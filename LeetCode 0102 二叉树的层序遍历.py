9f522ff9197e14584a5684ed9b2f9b815b62869e
qianlinyi <327120052@qq.com>
2023-03-15 10:38:48 +0800

    20230315 LeetCode 0102 二叉树的层序遍历 BFS

---
+++ LeetCode 0102 二叉树的层序遍历.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+import collections
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        q = collections.deque()
+        ans = []
+        if root is None:
+            return ans
+        q.append(root)
+        while q:
+            res, temp, q = [], q, []
+            for x in temp:
+                res.append(x.val)
+                if x.left:
+                    q.append(x.left)
+                if x.right:
+                    q.append(x.right)
+            ans.append(res)
+        return ans
