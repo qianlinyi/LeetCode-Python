560bb5f05a80690ea865b482879053f0d6b11f43
qianlinyi <327120052@qq.com>
2023-03-16 11:10:45 +0800

    20230316 LeetCode 0704 二分查找

---
+++ LeetCode 0704 二分查找.py
@@ -0,0 +1,10 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + r >> 1
+            if nums[mid] >= target:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l if l < len(nums) and nums[l] == target else -1
