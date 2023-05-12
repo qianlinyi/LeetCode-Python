2a033ce927167e5e3ab96c37fec91461c775f54e
qianlinyi <327120052@qq.com>
2023-05-12 11:47:38 +0800

    20230512 LeetCode 2673 使二叉树所有路径值相等的最小代价 贪心

---
+++ LeetCode 2673 使二叉树所有路径值相等的最小代价.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minIncrements(self, n: int, cost: List[int]) -> int:
+        ans = 0
+        for i in range(n // 2, 0, -1):  # 从最后一个非叶节点开始算
+            ans += abs(cost[i * 2 - 1] - cost[i * 2])  # 两个子节点变成一样的
+            cost[i - 1] += max(cost[i * 2 - 1], cost[i * 2])  # 累加路径和
+        return ans
