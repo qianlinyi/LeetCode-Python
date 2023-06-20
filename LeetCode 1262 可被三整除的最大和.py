0ccc83ce9d4b15ee67002a5d8ab338ea58464d85
qianlinyi <327120052@qq.com>
2023-06-20 15:50:19 +0800

    20230620 LeetCode 1262 可被三整除的最大和 DP

---
+++ LeetCode 1262 可被三整除的最大和.py
@@ -0,0 +1,12 @@
+from cmath import inf
+
+
+class Solution:
+    def maxSumDivThree(self, nums: List[int]) -> int:
+        f = [0, -inf, -inf]
+        for num in nums:
+            g = f[:]
+            for i in range(3):
+                g[(i + num % 3) % 3] = max(g[(i + num % 3) % 3], f[i] + num)
+            f = g
+        return f[0]
