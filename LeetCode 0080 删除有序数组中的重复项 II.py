df6f7560b8ac8d1fa0f5629b2e421b6e05c8caa2
qianlinyi <327120052@qq.com>
2023-09-12 11:06:52 +0800

    LeetCode 0080 删除有序数组中的重复项 II 双指针

---
+++ LeetCode 0080 删除有序数组中的重复项 II.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n = len(nums)
+        slow = fast = 2
+        while fast < n:
+            if nums[slow - 2] != nums[fast]:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        return slow
