c8338a57815d4314a9e388cc9daf5bab8ef9856b
qianlinyi <327120052@qq.com>
2023-11-10 12:09:11 +0800

    20231110 LeetCode 0135 分发糖果 遍历

---
+++ LeetCode 0135 分发糖果.py
@@ -0,0 +1,18 @@
+class Solution:
+
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        left = [0] * n
+        for i in range(n):
+            if i > 0 and ratings[i] > ratings[i - 1]:
+                left[i] = left[i - 1] + 1
+            else:
+                left[i] = 1
+        right = ans = 0
+        for i in range(n - 1, -1, -1):
+            if i < n - 1 and ratings[i] > ratings[i + 1]:
+                right += 1
+            else:
+                right = 1
+            ans += max(left[i], right)
+        return ans
