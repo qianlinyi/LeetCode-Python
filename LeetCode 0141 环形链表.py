3af950a1053d6186f77b481d6262f76138135549
qianlinyi <327120052@qq.com>
2024-07-28 21:45:56 +0800

    LeetCode 0141 环形链表 哈希表

---
+++ LeetCode 0141 环形链表.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        s = set()
+        while head:
+            if head in s:
+                return True
+            s.add(head)
+            head = head.next
+        return False
+
+
+if __name__ == '__main__':
+    head = ListNode(3)
+    head.next = ListNode(2)
+    head.next.next = ListNode(0)
+    head.next.next.next = ListNode(-4)
+    head.next.next.next.next = head
+    s = Solution()
+    print(s.hasCycle(head))
