862bf87244eb620c0270d42f86716bc4b8866f7f
qianlinyi <327120052@qq.com>
2024-10-04 01:08:55 +0800

    LeetCode 0146 LRU 缓存 双向链表+哈希表

--- LeetCode 0146 LRU 缓存.py
+++ LeetCode 0146 LRU 缓存.py
@@ -1,21 +1,81 @@
 import collections
 
 
-class LRUCache(collections.OrderedDict):
+# class LRUCache(collections.OrderedDict):
+#
+#     def __init__(self, capacity: int):
+#         super().__init__()
+#         self.capacity = capacity
+#
+#     def get(self, key: int) -> int:
+#         if key not in self:
+#             return -1
+#         self.move_to_end(key)
+#         return self[key]
+#
+#     def put(self, key: int, value: int) -> None:
+#         if key in self:
+#             self.move_to_end(key)
+#         self[key] = value
+#         if len(self) > self.capacity:
+#             self.popitem(last=False)
+
+class DLinkedListNode:
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
 
     def __init__(self, capacity: int):
-        super().__init__()
+        self.cache = dict()
+        self.head = DLinkedListNode()
+        self.tail = DLinkedListNode()
+        self.head.next = self.tail
+        self.tail.prev = self.head
         self.capacity = capacity
+        self.size = 0
 
     def get(self, key: int) -> int:
-        if key not in self:
+        if key not in self.cache:
             return -1
-        self.move_to_end(key)
-        return self[key]
+        node = self.cache[key]
+        self.moveToHead(node)
+        return node.value
 
     def put(self, key: int, value: int) -> None:
-        if key in self:
-            self.move_to_end(key)
-        self[key] = value
-        if len(self) > self.capacity:
-            self.popitem(last=False)
+        if key not in self.cache:
+            node = DLinkedListNode(key, value)
+            self.cache[key] = node
+            self.size += 1
+            self.addToHead(node)
+            if self.size > self.capacity:
+                removed = self.removeTail()
+                self.cache.pop(removed.key)
+                self.size -= 1
+        else:
+            node = self.cache[key]
+            node.value = value
+            self.moveToHead(node)
+
+    def addToHead(self, node):
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+
+    def removeNode(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def moveToHead(self, node):
+        self.removeNode(node)
+        self.addToHead(node)
+
+    def removeTail(self):
+        node = self.tail.prev
+        self.removeNode(node)
+        return node
